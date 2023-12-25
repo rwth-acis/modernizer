a9480f3bc185d93a5056fb14f26e5f1cb2d49c40
Ilija Kovacevic <ilijakovac1@googlemail.com>
2023-12-25 11:58:45 +0100

    add a sample API application to test docker build pipeline

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", getInfo)
+
+	router.Run(":8080")
+}
+
+func getInfo(c *gin.Context) {
+
+	resp, err := http.Get("https://modernizer.milki-psy.dbis.rwth-aachen.de")
+
+	if err != nil {
+		//
+	}
+
+	// Set the content type to text/plain
+	c.Header("Content-Type", "text/plain")
+
+	// Copy the HTML data directly to the response
+	_, err = io.Copy(c.Writer, resp.Body)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		//
+	}
+
+}
